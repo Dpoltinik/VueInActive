3444aba7e0a5ad95a026a064467b8a7cbe10dfd3
Dmitriy <d.poltinik@yandex.ru>
2022-05-31 20:28:11 +0300

    work with nuxt

--- part-11/itunes-search/nuxt.config.js
+++ part-11/itunes-search/nuxt.config.js
@@ -3,17 +3,19 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'itunes-search',
+    title: 'iTunes Search App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'iTunes search project' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300, 400, 500, 700|Material+Icons'}
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'}
     ]
   },
+  plugins: ['~plugins/vuetify.js'],
+  css: ['~assets/app.styl'],
   /*
   ** Customize the progress bar color
   */
@@ -22,9 +24,6 @@ module.exports = {
   ** Build configuration
   */
   build: {
-      vendor: ['axios', 'vuetify']
-  },
-  plugins: ['~plugins/vuetify.js'],
-  css: ['~assets/app.styl']
+    vendor: ['axios', 'vuetify']
+  }
 }
-
